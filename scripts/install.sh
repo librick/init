78c5af52375c5f077e1707f81ac747d86d2fd80a
Eric McDonald <ericmcdonald@protonmail.com>
2022-09-23 17:58:25 -0400

    make rust install noninteractive

--- scripts/install.sh
+++ scripts/install.sh
@@ -107,7 +107,7 @@ cp -r ./configs/.config/zsh/ $HOME/.config/
 # Install Rust's package manager, cargo:
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://sh.rustup.rs --proto '=https' --tlsv1.3 -sSf | sh -s -- -y
 source "$CARGO_HOME/env"
 # Use cargo to install lsd
 cargo install lsd

e7eee6499ad9635ddf958ee67c90ec67984c3c38
Eric McDonald <ericmcdonald@protonmail.com>
2022-09-23 18:42:25 -0400

    make all scripts executable

--- scripts/install-signal.sh
+++ scripts/install-signal.sh
@@ -6,3 +6,4 @@ cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo apt-get update && sudo apt-get install -y signal-desktop
+rm signal-desktop-keyring.gpg

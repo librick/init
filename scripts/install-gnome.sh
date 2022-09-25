27b42f56b2395e0d6830c0b2c115eeac25df0206
Eric McDonald <ericmcdonald@protonmail.com>
2022-09-25 17:08:36 -0400

    fix lsd font and enable pulseaudio

--- scripts/install-gnome.sh
+++ scripts/install-gnome.sh
@@ -4,10 +4,14 @@ xargs sudo apt-get install -y < ./pkg-files/gnome.txt
 sudo systemctl set-default graphical.target
 sudo apt-get purge -y --autoremove gnome-terminal nautilus-extension-gnome-terminal
 sudo apt-get install -y gnome-console nautilus-extension-gnome-console
+# Install sound software
+sudo apt-get install -y pulseaudio pulseaudio-utils pavucontrol
+systemctl --user enable --now pulseaudio
+systemctl --user restart --now pulseaudio
 # Copy profile picture
 cp -f ./configs/.face $HOME/.config/.face
 # Set default font
-gsettings set org.gnome.desktop.interface monospace-font-name "Fira Code 11"
+gsettings set org.gnome.desktop.interface monospace-font-name "Hack Nerd Font Mono Regular 11"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
 

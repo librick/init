d05ba2190e449eb6f351ed06a5599bfb7036ce98
Eric McDonald <ericmcdonald@protonmail.com>
2022-09-26 23:01:35 -0400

    break out gsettings

--- scripts/install-gnome.sh
+++ scripts/install-gnome.sh
@@ -2,7 +2,7 @@
 # Install Gnome
 xargs sudo apt-get install -y < ./pkg-files/gnome.txt
 sudo systemctl set-default graphical.target
-sudo apt-get purge -y --autoremove gnome-terminal nautilus-extension-gnome-terminal
+sudo apt-get purge -y --autoremove gnome-terminal nautilus-extension-gnome-terminal mpv
 sudo apt-get install -y gnome-console nautilus-extension-gnome-console
 # Install sound software
 sudo apt-get install -y pulseaudio pulseaudio-utils pavucontrol
@@ -10,11 +10,5 @@ systemctl --user enable --now pulseaudio
 systemctl --user restart --now pulseaudio
 # Copy profile picture
 cp -f ./configs/.face $HOME/.config/.face
-# Set default font
-gsettings set org.gnome.desktop.interface monospace-font-name "Hack Nerd Font Mono Regular 11"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gtk.Settings.FileChooser sort-directories-first true
-gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
-gsettings set org.gnome.desktop.app-folders folder-children "[]"
-
-
+# Set gsettings
+sh -c ./scripts/set-gsettings.sh
---
+++ scripts/set-gsettings.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Set various Gnome settings
+
+# Set default font
+gsettings set org.gnome.desktop.interface monospace-font-name "Hack Nerd Font Mono Regular 11"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
+# Disable grouping apps into folders in the overview
+gsettings set org.gnome.desktop.app-folders folder-children "[]"
+# gsettings set org.gnome.Cheese photo-path 'somepath'
+# gsettings set org.gnome.Cheese video-path 'somepath'
+# Set the calculator to "advanced" mode
+gsettings set org.gnome.calculator button-mode advanced
+# Set the calculator to disable requesting currency conversion rates
+gsettings set org.gnome.calculator refresh-interval 0
+# Disable automatic timezones
+gsettings set org.gnome.desktop.datetime automatic-timezone false
+# Set 24 hour clock
+gsettings set org.gnome.desktop.interface clock-format 24h
+gsettings set org.gtk.Settings.FileChooser clock-format 24h
+# Show the current day of the week
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+# Set font antialiasing to subpixel (rgba)
+gsettings set org.gnome.desktop.interface font-antialiasing rgba
+# Show battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+# Disable notifications on the lock screen
+gsettings set org.gnome.desktop.notifications show-in-lock-screen false
+# Use small icons in Nautilus
+gsettings set org.gnome.nautilus.icon-view default-zoom-level small
+# Use list view in Nautilus
+gsettings set org.gnome.nautilus.preferences default-folder-viewer list-view
+# Show hidden files in Nautilus
+gsettings set org.gnome.nautilus.preferences show-hidden-files true

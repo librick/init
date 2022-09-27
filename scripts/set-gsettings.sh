d4e6437e641ef84caedff6ae018ae8b85da55e61
Eric McDonald <ericmcdonald@protonmail.com>
2022-09-26 23:07:29 -0400

    clean up gsettings formatting

--- scripts/set-gsettings.sh
+++ scripts/set-gsettings.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # Set various Gnome settings
 
-# Set default font
-gsettings set org.gnome.desktop.interface monospace-font-name "Hack Nerd Font Mono Regular 11"
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+# Sort directories before files
 gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+# Right click by clicking the right side of the touchpad
 gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
 # Disable grouping apps into folders in the overview
 gsettings set org.gnome.desktop.app-folders folder-children "[]"
@@ -16,11 +15,16 @@ gsettings set org.gnome.calculator refresh-interval 0
 gsettings set org.gnome.desktop.datetime automatic-timezone false
 # Set 24 hour clock format
 gsettings set org.gnome.desktop.interface clock-format 24h
+# Set 24 hour clock format
 gsettings set org.gtk.Settings.FileChooser clock-format 24h
 # Show the current day of the week
 gsettings set org.gnome.desktop.interface clock-show-weekday true
+# Use dark color scheme
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 # Set font antialiasing to subpixel (rgba)
 gsettings set org.gnome.desktop.interface font-antialiasing rgba
+# Set default font
+gsettings set org.gnome.desktop.interface monospace-font-name "Hack Nerd Font Mono Regular 11"
 # Show battery percentage
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 # Disable notifications on the lock screen

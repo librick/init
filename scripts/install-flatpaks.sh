e98131dc7f1199902fe402009d21d0dfdc73f618
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-16 16:02:56 -0500

    add flatpak upgrades, install as user

--- scripts/install-flatpaks.sh
+++ scripts/install-flatpaks.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 # Install Flatpaks
+
+# Install flatpak binaries
+sudo apt-get update -y
+sudo apt-get autoremove --purge -y
 sudo apt-get install -y flatpak
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.mozilla.firefox
-flatpak install -y flathub com.brave.Browser
-flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub org.telegram.desktop
-flatpak install -y flathub org.blender.Blender
-flatpak install -y flathub md.obsidian.Obsidian
-flatpak install -y flathub com.rafaelmardojai.Blanket
-flatpak install -y com.github.tchx84.Flatseal
+
+# Install flatpaks via flatpak CLI
+flatpak remote-add -u --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -u -y flathub org.mozilla.firefox
+flatpak install -u -y flathub com.brave.Browser
+flatpak install -u -y flathub com.discordapp.Discord
+flatpak install -u -y flathub org.telegram.desktop
+flatpak install -u -y flathub org.blender.Blender
+flatpak install -u -y flathub md.obsidian.Obsidian
+flatpak install -u -y flathub com.rafaelmardojai.Blanket
+flatpak install -u -y com.github.tchx84.Flatseal
+
+# Install update service
+systemdUserDir=$XDG_CONFIG_HOME/systemd/user
+mkdir -p $systemdUserDir
+cp -f ./configs/.config/systemd/user/*.{service,timer} $systemdUserDir/
+systemctl --user enable --now update-user-flatpaks.timer
+
